6bab5e5a12513128eba1379e19d686f98e89f48f
algsupport <admin@algsupport.com>
2021-10-19 01:41:53 +0300

    fixed yii2 compatibility

--- src/Transport/GmailApiTransport.php
+++ src/Transport/GmailApiTransport.php
@@ -8,11 +8,7 @@
 
 	class GmailApiTransport
 	{
-<<<<<<< HEAD
 		private string $_google_api_credentials = "";
-=======
-		public string $google_api_credentials = "";
->>>>>>> ee5961962b6818e1c65aae2add6a4b48eda94da8
 
 		public Client $client;
 
@@ -30,13 +26,9 @@
 			$this->client->setAuthConfig($this->_google_api_credentials);
 			$this->client->addScope(Gmail::MAIL_GOOGLE_COM);
 	    }
-<<<<<<< HEAD
 
 		public function setGoogleApiCredentials($credentials)
         {
             $this->_google_api_credentials = $credentials;
         }
 	}
-=======
-	}
->>>>>>> ee5961962b6818e1c65aae2add6a4b48eda94da8
